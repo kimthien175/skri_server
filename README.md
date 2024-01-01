c5e0df9bc0c2b83cc937d8ce2dcb3f9aac85709b
root <kimthien175@gmail.com>
2024-01-01 22:05:52 +0000

    done conterizing project

---
+++ README.md
@@ -0,0 +1,3 @@
+BUILD IMAGE: docker-compose build
+RUN CONTAINER IN DETACHED MODE(IN THE BACKGROUND): docker-compose up -d
+STOP CONTAINER: docker-compose down
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,9 @@ import express from "express";
 const app = express();
 app.listen(4000, () => {
   console.log(`server runningggh on port 4000`);
+  console.log('yolo helele')
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!n hehehe");
 });
