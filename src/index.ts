8823673a4a3340f6305e27bf9bbdc7f7a13ba337
root <kimthien175@gmail.com>
2024-01-02 04:04:21 +0000

    use express with socket io

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 console.log('hello world')
 
-import express from "express"
-//import {Server} from "socket.io"
+import { Server } from "socket.io"
+import express = require('express')
+import {createServer} from 'http'
 
-const app = express();
-app.listen(4000, () => {
-  console.log(`server runningggh on port 4000`);
-  console.log('how are you today');
-});
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/news', (req, res)=>{
+  res.send('News')
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello World!n hehehe");
-});
+
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+io.on('connection', (socket) => {
+  //..
+})
+
+httpServer.listen(4000)
